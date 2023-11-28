d45c9663a7a412c283cefdffd463ac7c2a30c76d
Zachary Robinson <zrobins2@swarthmore.edu>
2023-11-27 22:59:36 -0500

    "Open" button interface variant Fixes #35

--- src/renderer/src/components/SlateCard.tsx
+++ src/renderer/src/components/SlateCard.tsx
@@ -1,7 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TestContext } from "@renderer/context/context";
 import { createRef, useContext } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import ContentEditable from "react-contenteditable";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function SlateCard(
   props: SlateFile & {
@@ -9,9 +11,10 @@ export default function SlateCard(
     onInnerNameChange?: (id: string, newName: string) => void;
   }
 ) {
-  const { id, fileName, fileType, tags, index, onInnerNameChange } = props;
-  
-  const testMode = useContext(TestContext)
+  const { id, fileName, fileType, tags, index, onInnerNameChange, filePath } =
+    props;
+
+  const testMode = useContext(TestContext);
 
   // SlateCard.tsx
   const bgColorClass =
@@ -27,7 +30,7 @@ export default function SlateCard(
       {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
-          className={`rounded p-2 mb-2 flex items-center justify-between ${bgColorClass} ${
+          className={`rounded p-2 mb-2 flex items-center justify-between z-30 ${bgColorClass} ${
             snapshot.isDragging ? "opacity-75" : "opacity-100"
           } shadow`}
           {...provided.draggableProps}
@@ -36,20 +39,32 @@ export default function SlateCard(
           <div className="flex flex-col justify-between w-full h-full">
             {onInnerNameChange ? (
               <ContentEditable
-                className="self-start font-detail"
+                className="self-start font-detail mb-1"
                 innerRef={ref}
                 html={fileName}
                 onChange={(e) => onInnerNameChange(id, e.target.value)}
                 tagName="h1"
               />
             ) : (
-              <p className="self-start font-detail">{fileName}</p>
+              <p className="self-start font-detail mb-1">{fileName}</p>
             )}
             {/* Spacer to push filetype to the bottom */}
             <div className="flex-grow"></div>
-            {/* Align filetype label to the bottom-right */}
-            <div className="self-end rounded bg-black bg-opacity-50 text-white p-1 font-label">
-              <p>.{fileType}</p>
+            <div className="flex flex-row">
+              <div className="flex-grow" />
+              {testMode == "button" ? (
+                <button
+                  className="rounded w-16 p-1 font-label text-sm text-white italic bg-slate-500 bg-opacity-50"
+                  onClick={() => window.files.openExternally(filePath)}
+                >
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> .
+                  {fileType}
+                </button>
+              ) : (
+                <div className="rounded p-1.5 font-label text-sm text-white italic bg-slate-500 bg-opacity-50">
+                  <p>.{fileType}</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
